61449612c7d4e9ebd7821090bc6e45c60e75738f
Manudithur <Manudithur@gmail.com>
2022-09-11 01:08:44 -0300

    Sign-In / Sign-Up and Landing page

---
+++ src/views/Home.vue
@@ -0,0 +1,140 @@
+<template>
+  <v-parallax dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" height="40%">
+    <v-app-bar app>
+      <v-toolbar-title>
+        <router-link to="/landing">
+          <v-img
+            lazy-src=""
+            max-height="100"
+            max-width="130"
+            src="../assets/FutSwap.png">
+        </v-img>
+        </router-link>
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-app id="inspire" class="transparent">
+    <v-content>
+      <v-container class="fill-height" fluid style="max-width:1500px;">
+        <v-row class="center" justify="center" >
+          <v-col cols="14" sm="8" md="8">
+            <v-card>
+              <v-window v-model="step" vertical>
+                <v-window-item :value="1" >
+                  <v-row>
+                    <v-col cols="14" md="7">
+                      <v-card-text class="mt-auto pa-5">
+                        <h1 class="text-center display-2 font-weight-bold blue--text text--darken-3 pa-5">Ingresa a tu cuenta</h1>
+                        <v-form>
+                          <v-text-field
+                            label="Email"
+                            name="Email"
+                            prepend-icon="email"
+                            type="text"
+                            color="blue darken-3"
+                          />
+                          <v-text-field
+                            id="password"
+                            label="Contraseña"
+                            name="password"
+                            prepend-icon="lock"
+                            type="password"
+                            color="blue darken-3"
+                          />
+                        </v-form>
+                        <a href="#" class="text-decoration-underline text-center">Olvidaste tu contraseña?</a>
+                        <div class="text-center mt-10">
+                        <v-btn rounded color="blue darken-3" dark>INGRESAR</v-btn>
+                        </div>
+
+                      </v-card-text>
+                    </v-col>
+                    <v-col cols="14" md="5" class="blue darken-3">
+                      <v-card-text class="white--text mt-auto">
+                        <h1 class="text-center display-1 font-weight-mid">¿Sos nuevo?</h1>
+                        <h5 class="text-center">Registrate ya y empeza a swapear!</h5>
+                        <div class="text-center pa-5">
+                        <v-btn rounded outlined dark @click="step++" class="">REGISTRARME</v-btn>
+                        </div>
+                      </v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row class="fill-height">
+                    <v-col cols="14" md="7" >
+                      <v-card-text class="mt-auto">
+                        <h1 class="text-center display-2 blue--text text--darken-3 font-weight-bold pa-5">¡Bienvenido!</h1>
+                        <v-form>
+                          <v-text-field
+                            label="Nombre"
+                            name="Name"
+                            prepend-icon="person"
+                            type="text"
+                            color="blue darken-3"
+                          />
+                          <v-text-field
+                            label="Email"
+                            name="Email"
+                            prepend-icon="email"
+                            type="text"
+                            color="blue darken-3"
+                          />
+
+                          <v-text-field
+                            id="password"
+                            label="Contraseña"
+                            name="password"
+                            prepend-icon="lock"
+                            type="password"
+                            color="blue darken-3"
+                          />
+                        </v-form>
+                      </v-card-text>
+                      <div class="text-center mt-n5 pa-5">
+                        <v-btn rounded color="blue darken-3" dark>Crear Cuenta</v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="14" md="5" class="blue darken-3">
+                      <v-card-text class="white--text mt-auto">
+                        <h1 class="text-center display-1">¿Ya sos parte de FutSwap?</h1>
+                        <div class="text-center pa-5">
+                        <v-btn rounded outlined dark @click="step--" class="mb-5">Ingresar</v-btn>
+                        </div>
+                      </v-card-text>                        
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+  </v-parallax>
+</template>
+<style>
+  html {
+    overflow: hidden !important;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+  }
+  
+  html::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+  }
+  </style>
+
+<script>
+export default {
+  data: () => ({
+    step: 1,
+    appTitle: 'FutSwap',
+  }),
+  props: {
+    source: String
+  },
+};
+
+</script>
---
+++ src/views/Landing.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-parallax dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" height="100%">
+      <v-app-bar app>
+        <v-toolbar-title>
+          <v-img
+                lazy-src=""
+                max-height="100"
+                max-width="130"
+                src="../assets/FutSwap.png"></v-img>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          {{ item.title }}
+        </v-btn>
+        </v-toolbar-items>
+      </v-app-bar>
+    <v-app id="inspire" class="transparent">
+      <h1 class="white--text ">LANDING PAGE NOT DEVELOPED</h1>
+    </v-app>
+    </v-parallax>
+  </template>
+  
+  <script>
+  export default {
+    data: () => ({
+      step: 1,
+      appTitle: 'FutSwap',
+        sidebar: false,
+        menuItems: [
+            {title: '¿Que es FutSwap?', path: ''},
+            { title: 'Ingresar / Registrame', path: '/'},   
+       ]
+    }),
+    props: {
+      source: String
+    },
+  };
+  
+  </script>
