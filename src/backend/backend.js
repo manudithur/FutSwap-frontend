c4ceb4378cd4f33b20d286fcbf496535c7443fc0
Juan Ramiro Castro <juancastro@itba.edu.ar>
2022-11-21 18:12:57 -0300

    FUT-110

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -1,4 +1,4 @@
-import {getFirestore} from "@/backend/fireGetters";
+import {getFirestore, getFunctions} from "@/backend/fireGetters";
 import {collection, query, where, getDocs, orderBy, limit} from 'firebase/firestore';
 import {validateUserID} from "@/backend/validation";
 
@@ -39,7 +39,7 @@ export async function getActiveMarketPosts(uid, limitNum) {
  *              closed: timestamp,
  *              buyer: string,
  *              comment: string,
- *              seller: string }[]>}
+ *              seller: string[]}>}
  */
 export async function getInactiveSoldBy(uid) {
     validateUserID(uid);
@@ -78,4 +78,57 @@ export async function getInactiveBoughtBy(uid) {
         array.push(doc.data());
     });
     return array;
+}
+
+/**
+ * Deletes market post with corresponding id.
+ * @param {string} postID
+ * @returns {Promise<void>}
+ */
+export async function deleteMarketPost(postID) {
+    const functions = getFunctions();
+
+    const deletePost = functions.httpsCallable('deleteMarketPost');
+    deletePost({
+        postId: postID
+    })
+}
+
+/**
+ * Creates market post with price, figus and amount. If amount for a figu is not present, assumes 1
+ * Returns postId of new post
+ * @param {number} price
+ * @param {string[]} figus
+ * @param {number[]} amounts
+ * @returns {Promise<string>}
+ */
+export async function createMarketPost(price, figus, amounts) {
+    const functions = getFunctions();
+
+    const createPost = functions.httpsCallable('createMarketPost');
+    createPost({
+        price: price,
+        figus: figus,
+        amounts: amounts
+    })
+}
+
+/**
+ * Modifies an existing market post. If amount for a figu is not present, assumes 1
+ * @param {string} postID
+ * @param {number} price
+ * @param {string[]} figus
+ * @param {number[]} amounts
+ * @returns {Promise<void>}
+ */
+export async function updateMarketPost(postID, price, figus, amounts) {
+    const functions = getFunctions();
+
+    const updatePost = functions.httpsCallable('updateMarketPost');
+    updatePost({
+        postId: postID,
+        price: price,
+        figus: figus,
+        amounts: amounts
+    })
 }
