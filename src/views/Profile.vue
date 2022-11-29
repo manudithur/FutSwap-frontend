1e0c09c364e04b8a9f93c48ab6b389fa3fdec208
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2022-11-29 10:52:59 -0300

    feat: El rating de profile + otros arreglos 💀💀💀

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,74 +1,42 @@
 <template>
   <v-app fill-height app>
-    <NavBar />
+    <NavBar/>
     <v-main app class="content align-center pt-10 ma-0">
-      <v-row
-        class="fill-height justify-center align-center pa-5 ma-0"
-        v-if="isLoading"
-      >
-        <v-progress-circular
-          indeterminate
-          color="primary"
-          :size="70"
-          :width="7"
+      <v-row class="fill-height justify-center align-center pa-5 ma-0" v-if="isLoading">
+        <v-progress-circular indeterminate color="primary" :size="70" :width="7"
         ></v-progress-circular>
       </v-row>
-      <v-row
-        dense
-        class="fill-height justify-center align-center pa-5 ma-0"
-        v-if="!isLoading"
-      >
+      <v-row dense class="fill-height justify-center align-center pa-5 ma-0" v-if="!isLoading">
         <v-col cols="3">
-          <v-card
-            class="text-center justify-center align-center white mx-3 mb-3 pa-3"
-          >
+          <v-card class="text-center justify-center align-center white mx-3 mb-3 pa-3">
             <v-card-title class="justify-center">
-              <v-badge
-                avatar
-                bordered
-                bottom
-                content=""
-                color="var(--darkblue)"
-                icon=""
-                overlap
-                offset-x="32"
-                offset-y="32"
-              >
+              <v-badge avatar bordered bottom content="" color="var(--darkblue)" icon="" overlap offset-x="32"
+                       offset-y="32">
                 <template v-slot:badge>
-                  <v-icon size="20" style="z-index: 1; height:26px !important; width: 26px !important;" @click="handleFileImport">mdi-tray-arrow-up</v-icon>
+                  <v-icon size="20" style="z-index: 1; height:26px !important; width: 26px !important;"
+                          @click="handleFileImport">mdi-tray-arrow-up
+                  </v-icon>
                 </template>
-                <v-avatar
-                  circle
-                  size="150"
-                  :loading="isSelecting"
-                >
+                <v-avatar circle size="150" :loading="isSelecting">
                   <input ref="uploader" class="d-none" type="file" accept="image/jpeg" @change="onFileChanged"/>
-                  <v-img v-if="profilePictureUrl" :src="profilePictureUrl" />
+                  <v-img v-if="profilePictureUrl" :src="profilePictureUrl"/>
                 </v-avatar>
               </v-badge>
             </v-card-title>
-            <p class="pt-5 pb-0 mb-0">RATING:</p>
-            <v-rating
-              background-color="var(--gold)"
-              color="var(--gold)"
-              empty-icon="mdi-star-outline"
-              full-icon="mdi-star"
-              half-icon="mdi-star-half-full"
-              half-increments
-              length="5"
-              readonly
-              size="20"
-              value="4.5"
-            ></v-rating>
+            <div style="height: 80px">
+              <v-progress-circular v-if="isLoadingRating" :indeterminate="true" class="mt-4"/>
+              <p v-else-if="ratingError">{{ ratingError }}</p>
+              <template v-else>
+              <p class="pt-5 pb-0 mb-0">CALIFICACIÓN:</p>
+              <v-rating background-color="var(--gold)" color="var(--gold)" empty-icon="mdi-star-outline"
+                        full-icon="mdi-star" half-icon="mdi-star-half-full" half-increments length="5"
+                        readonly size="20" :value="rating"/>
+              </template>
+            </div>
           </v-card>
           <v-card class="text-center align-center white mx-3 mb-3 pa-3">
             <h1>PROGRESO</h1>
-            <v-progress-linear
-              v-model="progress"
-              color="rgb(62,77,124)"
-              height="25"
-              >{{ progress }}%
-            </v-progress-linear>
+            <v-progress-linear v-model="progress" color="rgb(62,77,124)" height="25">{{ progress }}%</v-progress-linear>
           </v-card>
           <v-card class="text-center align-center white ma-3 pa-3">
             <h1>SWAPS</h1>
@@ -78,22 +46,13 @@
         <v-col cols="6 ma-0">
           <v-card class="text-center ma-0 align-center justify-center pa-5">
             <v-card-text>
-              <v-text-field v-model="name" outlined label="Nombre" />
-              <v-text-field
-                v-model="email"
-                disabled
-                outlined
-                label="Correo Electronico"
-              />
-              <v-text-field v-model="phone" outlined label="Telefono" />
-              <v-text-field v-model="address" outlined label="Direccion" />
+              <v-text-field v-model="name" outlined label="Nombre"/>
+              <v-text-field v-model="email" disabled outlined label="Correo Electronico"/>
+              <v-text-field v-model="phone" outlined label="Telefono"/>
+              <v-text-field v-model="address" outlined label="Direccion"/>
             </v-card-text>
             <v-card-actions class="justify-center pb-5 pt-n5">
-              <v-btn
-                color="rgb(62,77,124)"
-                style="color: white"
-                @click="update"
-              >
+              <v-btn color="rgb(62,77,124)" style="color: white" @click="update">
                 Guardar cambios
                 <v-icon left dark>check</v-icon>
               </v-btn>
@@ -102,7 +61,7 @@
         </v-col>
       </v-row>
     </v-main>
-    <FooterBar />
+    <FooterBar/>
   </v-app>
 </template>
 
@@ -143,13 +102,14 @@ import Swal from "sweetalert2";
 import {
   getCurrentUser,
   getUserPrivateProfileAsync,
+  getUserProfilePictureAsync,
   getUserPublicProfileAsync,
+  getUserRatingAsync,
+  signOutAsync,
   updateUserPrivateProfileAsync,
   updateUserPublicProfileAsync,
-  getUserProfilePictureAsync,
   uploadProfilePicture,
-  signOutAsync,
-} from "../backend/users";
+} from "@/backend/users";
 import NavBar from "../components/NavBar.vue";
 import FooterBar from "../components/FooterBar.vue";
 
@@ -166,6 +126,10 @@ export default {
     isLoadingProfileUrl: false,
     profilePictureUrl: null,
 
+    isLoadingRating: false,
+    rating: null,
+    ratingError: null,
+
     isUploadingProfilePicture: false,
     profileUploadFile: null,
   }),
@@ -176,17 +140,26 @@ export default {
     // Empiezo a buscar el dato a la API. Esta tarea queda corriendo en el fondo.
     this.loadData();
     this.loadProfilePicture();
+    this.loadRating();
   },
   methods: {
     async loadData() {
       try {
         const user = getCurrentUser();
         this.email = user.email;
-        const publicData = await getUserPublicProfileAsync(user.uid);
+        const publicProfilePromise = getUserPublicProfileAsync(user.uid);
+        const privateProfilePromise = getUserPrivateProfileAsync();
+        const publicData = await publicProfilePromise;
+        const privateData = await privateProfilePromise;
+
         this.name = publicData.displayName;
         this.phone = publicData.phone;
-        const privateData = await getUserPrivateProfileAsync();
-        this.address = privateData.address;
+        if (privateData.address)
+          this.address = privateData.address;
+        else if (privateData.location)
+          this.address = privateData.location.toString();
+        else
+          this.address = '';
 
         // ~~PROVISORIO~~ PARA HACER AUTOMÁTICO EL PASAJE DE user.displayName A publicProfile.displayName
         if (!publicData.displayName || publicData.displayName == '') {
@@ -208,6 +181,19 @@ export default {
       }
     },
 
+    async loadRating() {
+      this.isLoadingRating = true;
+      try {
+        this.rating = (await getUserRatingAsync(getCurrentUser().uid)).average;
+      } catch (e) {
+        console.log("Ratings F 💀", e);
+        this.rating = null;
+        this.ratingError = "No pudimos cargar tu calificación 💀";
+      } finally {
+        this.isLoadingRating = false;
+      }
+    },
+
     async onFileChanged(e) {
       if (this.isUploadingProfilePicture) {
         alert('Por favor espere a que termine de subir el archivo anterior');
@@ -219,7 +205,7 @@ export default {
       this.profileUploadFile = e.target.files[0];
       try {
         this.profilePictureUrl = await uploadProfilePicture(this.profileUploadFile);
-      } catch(e) {
+      } catch (e) {
         console.log(e);
         alert('No pudimos subir tu foto de perfil. Asegurate que pese menos de 5MBs!');
       } finally {
@@ -235,8 +221,14 @@ export default {
 
     update: async function () {
       try {
-        await updateUserPublicProfileAsync({displayName: this.name, phone: this.phone ? this.phone : ""}, true);
-        await updateUserPrivateProfileAsync({address: this.address ? this.address : ""}, true);
+        const updatePublicPromise = updateUserPublicProfileAsync({
+          displayName: this.name,
+          phone: this.phone ? this.phone : ""
+        }, true);
+        const updatePrivatePromise = updateUserPrivateProfileAsync({address: this.address ? this.address : ""}, true);
+        await updatePublicPromise;
+        await updatePrivatePromise;
+
         await Swal.fire({
           position: 'center',
           icon: 'success',
@@ -247,7 +239,7 @@ export default {
         console.log(e);
         // Si hubo algun error...
         const errorMessage =
-          "Error al actualizar perfil. Reintentar. Si persiste el error contactar soporte";
+            "Error al actualizar perfil. Reintentar. Si persiste el error contactar soporte";
         await Swal.fire({
           position: "center",
           icon: "error",
@@ -260,16 +252,16 @@ export default {
       this.isSelecting = true;
       // After obtaining the focus when closing the FilePicker, return the button state to normal
       window.addEventListener(
-        "focus",
-        () => {
-          this.isSelecting = false;
-        },
-        { once: true }
+          "focus",
+          () => {
+            this.isSelecting = false;
+          },
+          {once: true}
       );
       // Trigger click on the FileInput
       this.$refs.uploader.click();
     },
   },
-  components: { NavBar, FooterBar },
+  components: {NavBar, FooterBar},
 };
 </script>
